d1de0b9011eff7efb144ba542b78645cd5a5830c
keerthanakannan29 <42834923+keerthanakannan29@users.noreply.github.com>
2019-12-19 22:14:42 -0800

    Add files via upload

---
+++ AddressBook-MVC-java-master/src/mvc/models/Person.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc.models;
+
+/**
+ *
+ * @author Admin
+ */
+public class Person {
+    private String name;
+    private String mob;
+    private String email;
+    private String address;
+    private String pincode;
+    
+   /* public User(){
+        x=0;
+    }
+    
+    public User(int x){
+        this.x=x;
+    }
+    
+    public void incX(){
+        x++;
+    }
+    
+    public void decX(){
+        x--;
+    }
+    public int getX(){
+        return x;
+    }*/
+    public String getName(){
+        return this.name; 
+   }
+    
+    public String getMob(){
+        return this.mob; 
+   }
+    
+    public String getEmail(){
+        return this.email; 
+   }
+    public String getAddress(){
+        return this.address; 
+   }
+     public String getPincode(){
+        return this.pincode; 
+   }
+    
+    public void setData(String name, String mob,String email,String address){
+        this.name = name;
+        this.mob = mob;
+        this.email = email;
+        this.address = address;
+        this.pincode = pincode;
+    }
+
+    public void setData(String name, String mobile, String email, String address, String pincode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+ 
+
+}
